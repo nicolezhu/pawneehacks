97c0b6abd84230d209058baaee46a4017e98bb88
Nicole Zhu <nzhu94@gmail.com>
2017-09-09 12:16:16 -0400

    add singular sandwich support

--- source/content_script.js
+++ source/content_script.js
@@ -1,8 +1,10 @@
 var sandwichReplacements = ['sammies', 'sandoozles', 'Adam Sandlers'];
+var sandwichReplacementsSingular = ['sammies', 'sandoozles', 'Adam Sandlers'];
 var numSandwiches = sandwichReplacements.length;
 
 findAndReplace(/\bdesserts\b/ig, 'zerts', document);
 findAndReplace(/\bentrees\b/ig, 'tray-trays', document);
+findAndReplace(/\bsandwich\b/ig, sandwichReplacementsSingular[Math.floor(Math.random()*numSandwiches)], document);
 findAndReplace(/\bsandwiches\b/ig, sandwichReplacements[Math.floor(Math.random()*numSandwiches)], document);
 findAndReplace(/\bair conditioners\b/ig, 'cool blasterz', document);
 findAndReplace(/\bnoodles\b/ig, 'long-ass rice', document);
